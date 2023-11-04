249acd698f3201526e0c17516219313f87bbb035
tron01 <69385258+tron01@users.noreply.github.com>
2023-11-04 18:30:08 +0530

    user section
    profile ,about ,ipc

--- User/user_views.py
+++ User/user_views.py
@@ -9,7 +9,7 @@ from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
-conn = MySQLdb.connect("localhost","root","","legal_advisor")
+conn = MySQLdb.connect("localhost","root","","law")
 c = conn.cursor()
 
 
@@ -28,3 +28,27 @@ def user_header_footer(request):
     if 'uid' not in request.session:
         return HttpResponseRedirect("/login")
     return render(request,"user_header_footer.html")
+
+
+def user_about(request):
+    return render(request,"user_about.html")
+
+def user_profile(request):
+    u_id = request.session["uid"]
+    s = "select * from user u , login l where u.u_id = '"+str(u_id)+"' and u.u_id = l.user_id and l.type = 'user' "
+    c.execute(s)
+    data = c.fetchone()
+    print(data)
+    return render(request,"user_profile.html",{"data":data})
+
+def user_ipc(request):
+    s = "select * from ipc order by ipc_section"
+    # s = "SELECT *,BIN(ipc_section) AS binray_not_needed_column FROM ipc ORDER BY binray_not_needed_column ASC , ipc_section ASC"
+    # s = "SELECT *  CAST(ipc_section as SIGNED) AS casted_column FROM ipc ORDER BY casted_column ASC , ipc_section ASC"
+    print(s)
+    c.execute(s)
+    data =c.fetchall()
+    print(data)
+    # if 'login' in request.POST:
+        # return HttpResponseRedirect("/login")
+    return render(request,"user_ipc.html",{"data":data})
